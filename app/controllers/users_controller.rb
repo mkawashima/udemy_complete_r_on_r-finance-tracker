2eb975af89588f4fd4ee5518e083c990daa573c1
masafumi <l mkawashima@1stsolution.co.jp>
2020-07-22 16:39:56 +0900

    friends search form 追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,26 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search
+    if params[:friend].present?
+      @friend = User.search(params[:friend])
+      if @friend
+        respond_to do |format|
+          format.js {render partial: 'users/friend_result'}
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid name or email to search"
+          format.js {render partial: 'users/friend_result'}
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js {render partial: 'users/friend_result'}
+      end
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,8 @@ class User < ApplicationRecord
     "Anonymous"
   end
 
+  def self.search(params)
+    User.last
+  end
+
 end
